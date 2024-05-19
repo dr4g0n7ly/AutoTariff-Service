5200d158c643e7586aa1b519d8647c3eff5fa56e
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-05-19 11:48:46 +0530

    Abstract Kafka producer

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,29 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/dr4g0n7ly/AutoTariff-Service/types"
 	"github.com/gorilla/websocket"
 )
 
 var kafkaTopic = "obudata"
 
-func init() {
-	fmt.Println("init")
-}
-
 func main() {
-	fmt.Println("starting server 0")
 	recv, err := NewDataReceiver()
 	if err != nil {
 		print("fucked")
 		log.Fatal(err)
 	}
-	fmt.Println("starting server")
+	fmt.Println("starting data receiver")
 	http.HandleFunc("/ws", recv.handleWS)
 	http.ListenAndServe(":30000", nil)
 }
@@ -31,7 +25,7 @@ func main() {
 type DataReceiver struct {
 	msgch chan OBUData
 	conn  *websocket.Conn
-	prod  *kafka.Producer
+	prod  DataProducer
 }
 
 type OBUData struct {
@@ -41,45 +35,19 @@ type OBUData struct {
 }
 
 func NewDataReceiver() (*DataReceiver, error) {
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	p, err := NewKafkaProducer()
 	if err != nil {
 		print("ERROR CREATING NEW PRODUCER")
 		return nil, err
 	}
-
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-				}
-			}
-		}
-	}()
-
 	return &DataReceiver{
 		msgch: make(chan OBUData, 128),
 		prod:  p,
 	}, nil
 }
 
-func (dr *DataReceiver) produceData(data OBUData) error {
-	b, err := json.Marshal(data)
-	if err != nil {
-		print("ERROR PARSING DATA")
-		return err
-	}
-	err = dr.prod.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{
-			Topic:     &kafkaTopic,
-			Partition: kafka.PartitionAny,
-		},
-		Value: b,
-	}, nil)
-	return err
+func (dr *DataReceiver) produceData(data types.OBUData) error {
+	return dr.prod.ProduceData(data)
 }
 
 func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -100,7 +68,7 @@ func (dr *DataReceiver) wsReceiveLoop() {
 	fmt.Println("New OBU connected client connected")
 
 	for {
-		var data OBUData
+		var data types.OBUData
 		if err := dr.conn.ReadJSON(&data); err != nil {
 			log.Println("read error:", err)
 			continue
